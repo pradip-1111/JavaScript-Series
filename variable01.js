3dd6bc05dca0d9ecca536280585764f7202dc483
pradip-1111 <150052650+pradip-1111@users.noreply.github.com>
2024-09-17 09:45:40 +0000

    config

---
+++ variable01.js
@@ -0,0 +1,12 @@
+const accountId  = 1234;
+let accountEmail  = "pradip121@gmail.com"
+var accountPassword = "85238"
+accountCity = "nepal"
+
+// accountId = 23;
+accountEmail  = "ranjit121@gmail.com"
+accountPassword = "00000"
+// console.log(accountEmail)
+// console.log(accountId)
+// console.log(accountPassword)
+ console.table([accountEmail,accountId,accountPassword,accountCity])
