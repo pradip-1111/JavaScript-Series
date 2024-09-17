6db0db0647b61f5fc18e15f706d8fa72a0a8f817
pradip-1111 <150052650+pradip-1111@users.noreply.github.com>
2024-09-17 16:52:56 +0000

    conversion

---
+++ confersion.js
@@ -0,0 +1,23 @@
+let value = "2392abc"
+
+let score = Number(value)
+console.log(typeof value)  //string
+console.log(typeof score)  //number
+console.log(value)  
+console.log(score)  //NaN
+
+
+// "3423" => 3423
+// "365425abc"  => NaN
+// null => 0
+// undefined => NaN
+
+
+//for boolean
+
+// "" => false
+// 1 => true
+// "Pradip" => true
+
+
+
---
+++ datatypes.js
@@ -0,0 +1,19 @@
+"use stricts";
+
+// alert("hello")
+
+console.log(6+6)
+
+// number  => 2 to power 53
+// bigint
+// String   => ""
+// boolean => true/false
+// null => standalone value
+// undefined 
+// symbol => unique
+
+// object
+
+
+console.log(typeof null)   // object
+console.log(typeof undefined)  //undefined
