a7fe97a5b192a8ddeee9271a58b47f3323974863
pradip-1111 <150052650+pradip-1111@users.noreply.github.com>
2024-09-17 17:20:08 +0000

    addingfsdklfj

--- confersion.js
+++ confersion.js
@@ -1,10 +1,10 @@
 let value = "2392abc"
 
 let score = Number(value)
-console.log(typeof value)  //string
-console.log(typeof score)  //number
-console.log(value)  
-console.log(score)  //NaN
+// console.log(typeof value)  //string
+// console.log(typeof score)  //number
+// console.log(value)  
+// console.log(score)  //NaN
 
 
 // "3423" => 3423
@@ -18,6 +18,23 @@ console.log(score)  //NaN
 // "" => false
 // 1 => true
 // "Pradip" => true
+
+let a = "pradip"
+let b = " Sah"
+console.log(a+b) 
+
+// console.log("1"+2) //12
+// console.log(1+"2")  //12
+// console.log(1+"2"+2)  //122
+// console.log(1+2+"2")  //32
+// console.log("1"+2+2)  //122
+// console.log("1"+"2"+2)  //122
+
+
+let gamecounter = 100;
+++gamecounter  //101
+console.log(gamecounter)
+
 
 
 
