1255df9584e5c0c00a17aae350774ed5ef64081a
pradip-1111 <150052650+pradip-1111@users.noreply.github.com>
2024-09-28 20:19:34 +0000

    obj

--- Object.js
+++ Object.js
@@ -30,10 +30,60 @@ user.email = "ranjit123@gmail.com"
 user.greeting = function(){
     console.log("hello i am pradip ");
 }
-console.log(user.greeting())
+// console.log(user.greeting())
 
 
 user.greeting2 = function(){
     console.log(`hello i am pradip ,${this.age}`);
 }
-console.log(user.greeting2())
+// console.log(user.greeting2())
+
+
+// const tinderuser =  new Object() this is a singleton object 
+const tinderuser = {} // this is non singleton object 
+tinderuser.id = "123"
+tinderuser.name = "pradip"
+tinderuser.location = "india"
+// console.log(tinderuser)
+
+const regularuser = {
+    email :"sarrafpradeepgmail.com",
+    username :{
+        fullname:{
+            firstname:"pradip",
+            lastname:"sonar"
+        }
+    }
+}
+
+// console.log(regularuser.username.fullname) 
+// if there is nesterd object then we can print all the nested using .first we should use main object then which object we need to print
+
+const obj1 = {1:"a",2:"b",3:"c"}
+const obj2 = {4:"d",5:"e",6:"f"}
+// console.log(obj1,obj2)
+const obj3 = Object.assign(obj1,obj2)
+// console.log(obj3) // this assign helps to merge all the all the object in one object
+
+
+
+// when the object are in the arrays form
+const user2 = [
+    {
+        id:1,
+        name:"pradip",
+    },
+    {
+        id:1,
+        name:"pradip",
+    },
+    {
+        id:1,
+        name:"pradip",
+    }
+]
+
+user2[1].name
+console.log(Object.keys(tinderuser))
+console.log(Object.values(tinderuser))
+console.log(tinderuser.hasOwnProperty('location')) // this property helps to check that it has that keys or not 
